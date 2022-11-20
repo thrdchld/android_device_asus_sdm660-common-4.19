470770c1cbf0a99b9fba51fb910099def967d493
Bruno Martins <bgcngm@gmail.com>
2022-11-20 19:18:06 +0530

    sdm660-common: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c
    
    jasmine_sprout: Remove QTI perfd
    
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4
    
    jasmine_sprout: Declare BOARD_VENDOR
    
    * Needed for xiaomi-perfmgr power AIDL guards
    
    Change-Id: Idbb48e0f9b01090a456ad5d3f2d51f4dfab55672
    
    jasmine_sprout: Create dummy libqti-perfd-client
    
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    [SebaUbuntu]: Cleanup Android.bp and add copyright header
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    
    jasmine_sprout: libqti-perfd-client: Remove namespace declaration
    
    It's pointless when using extern "C".
    
    Change-Id: Ibdf9f06a70aa3a75687b33781c78cf2172bb334d
    
    jasmine_sprout: libqti-perfd-client: return a dummy value
    
    Return a positive integer for perf lock acquire and release so
    that Goodix/FPC fingerprint sensor blobs will not complain.
    
    Goodix:
    E [goodixHAL][gf_hal_milan_f_series]: goodix_perf_lock_acquire: Failed to acquire perf lock, err: 0
    E [goodixHAL][gf_hal_milan_f_series]: goodix_perf_lock_release: Perf lock release error 0
    
    FPC:
    E fpc_tac : fpc_perf_lock_acquire: Incorrect params, Failed to acquire perf lock, err
    E fpc_tac : fpc_perf_lock_release: Perf lock release error 0
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I861672e9a738c2204755d802670f4b28b662f286
    
    jasmine_sprout: libqti-perfd-client: Move to C
    
    * Why bothering with C++ mangling when we can just build it as a standard C library?
    
    Change-Id: I45ea977edf7ea7fab6fece76f3049654a8d24c5d
    
    jasmine_sprout: Add powerhint.json jasmine
    
    Change-Id: If270fc906fd833bb6b0d3b00621c0b079346e47d
    
    jasmine_sprout: rootdir: Initial libperfmgr init config
    
    * From coral, heavily stripped down
    
    Change-Id: Ib6846e4f4f23baf56910d2330e26846b99830218
    
    jasmine_sprout: sepolicy: Initial libperfmgr sepolicy
    
    Change-Id: I3d0a259bc89b4ecd0bf1632199172710d98230e0
    
    jasmine_sprout: Don't explicitly build older minor version HIDL libs
    
    Change-Id: Ib0caa49054a79b1f01ffc751826286a29c642e06
    
    jasmine_sprout: Remove QTI perf reminants
    
    jasmine_sprout: Add back ro.vendor.extension_library definition
    
     * We aren't using QCOM's perfd but our blobs still check for this.
     * To avoid unnecessary errors, let's just add it back.
    
    Change-Id: Icc44b330f21a658cfa8ded691a6628d8f62c3649
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -343,7 +343,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-    
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/privapp-permission/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
@@ -396,13 +396,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.asus_sdm660-libperfmgr
 
 # Powerhint
-ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
-    PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/power-libperfmgr/sdm636_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-else
-    PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-endif
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/powerhint/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Perf
 PRODUCT_PACKAGES += \
