636ba71992cf77bef31bef72baa23a0897f88314
Matt Buckley <mattbuckley@google.com>
2022-11-22 22:20:25 +0000

    sdm660-common: power-libperfmgr: Remove wakeup behavior from PowerHintSession and PowerSessionManager
    Currently, all sessions get boosted any time DISPLAY_UPDATE_IMMINENT is
    sent from SurfaceFlinger which can lead to large, unnecessary boosts.
    This patch aims to change that by removing the wakeup behavior, relying
    instead on sessions to boost themselves with new load change hints.
    
     * Remove wakeup() from PowerHintSession
     * Remove wakeSessions from PowerSessionManager
     * Remove related timers and message handlers
     * Remove DISPLAY_UPDATE_IMMINENT behavior entirely
    
    Test: manual
    Bug: b/260136431
    Change-Id: I4610edfefe8fcbef7d4cdbf5768830a9392a54f7

--- power-libperfmgr/aidl/PowerSessionManager.cpp
+++ power-libperfmgr/aidl/PowerSessionManager.cpp
@@ -96,16 +96,6 @@ void PowerSessionManager::updateHintBoost(const std::string &boost, int32_t dura
     ATRACE_CALL();
     ALOGV("PowerSessionManager::updateHintBoost: boost: %s, durationMs: %d", boost.c_str(),
           durationMs);
-    if (boost.compare("DISPLAY_UPDATE_IMMINENT") == 0) {
-        PowerHintMonitor::getInstance()->getLooper()->sendMessage(mWakeupHandler, NULL);
-    }
-}
-
-void PowerSessionManager::wakeSessions() {
-    std::lock_guard<std::mutex> guard(mLock);
-    for (PowerHintSession *s : mSessions) {
-        s->wakeup();
-    }
 }
 
 int PowerSessionManager::getDisplayRefreshRate() {
@@ -199,10 +189,6 @@ void PowerSessionManager::handleMessage(const Message &) {
     } else {
         enableSystemTopAppBoost();
     }
-}
-
-void PowerSessionManager::WakeupHandler::handleMessage(const Message &) {
-    PowerSessionManager::getInstance()->wakeSessions();
 }
 
 void PowerSessionManager::dumpToFd(int fd) {
