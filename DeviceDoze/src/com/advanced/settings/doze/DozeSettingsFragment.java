f6f0a49d331561daa14ba3596a1a5c8fd9bf983a
SonicBSV <sonicbsv@gmail.com>
2024-04-14 21:41:25 +0300

    sdm660-common: DeviceDoze: Migrate to CompoundButton.OnCheckedChangeListener device/asus/sdm660-common/DeviceDoze/src/com/advanced/settings/doze/DozeSettingsFragment.java:36: error: cannot find symbol import com.android.settingslib.widget.OnMainSwitchChangeListener;                                      ^   symbol:   class OnMainSwitchChangeListener   location: package com.android.settingslib.widget device/asus/sdm660-common/DeviceDoze/src/com/advanced/settings/doze/DozeSettingsFragment.java:39: error: cannot find symbol         OnMainSwitchChangeListener {         ^   symbol: class OnMainSwitchChangeListener
    https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/87f8cc1800e5b650ac83cf7e0cf8706a97bd4151

--- DeviceDoze/src/com/advanced/settings/doze/DozeSettingsFragment.java
+++ DeviceDoze/src/com/advanced/settings/doze/DozeSettingsFragment.java
@@ -30,13 +30,12 @@ import androidx.preference.SwitchPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import android.widget.Switch;
+import android.widget.CompoundButton;
 
 import com.android.settingslib.widget.MainSwitchPreference;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
 public class DozeSettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
-        OnMainSwitchChangeListener {
+        CompoundButton.OnCheckedChangeListener {
 
     private MainSwitchPreference mSwitchBar;
 
@@ -106,7 +105,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
     }
 
     @Override
-    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         Utils.enableDoze(getActivity(), isChecked);
         Utils.checkDozeService(getActivity());
 
