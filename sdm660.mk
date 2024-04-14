9ba87d31651937db90fbf35b460d2f49f730dc81
SonicBSV <sonicbsv@gmail.com>
2024-04-14 07:16:43 +0300

    sdm660-common: switch to android.hardware.usb@1.3-service-qti

--- sdm660.mk
+++ sdm660.mk
@@ -522,7 +522,7 @@ PRODUCT_SHIPPING_API_LEVEL := 27
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
