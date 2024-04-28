49f8c732d7cc9b2fdf0e9588940b34aaac7a3d3e
SonicBSV <sonicbsv@gmail.com>
2024-04-28 16:45:14 +0300

    sdm660-common: update bluetooth rules *up interfaces

--- sdm660.mk
+++ sdm660.mk
@@ -85,8 +85,9 @@ TARGET_BOOTANIMATION_HALF_RES := true
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
-    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
